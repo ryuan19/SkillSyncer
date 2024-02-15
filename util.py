81bf56fcfd69d45d9eb0218bb58dee1d22cdc80e
krrishchawla <krrishchawla1@gmail.com>
2024-02-14 21:20:50 -0800

    Updates to util functions

--- util.py
+++ util.py
@@ -9,7 +9,8 @@ from tqdm import tqdm
 from PyPDF2.errors import PdfReadError
 from InstructorEmbedding import INSTRUCTOR
 
-myModel = INSTRUCTOR('hkunlp/instructor-xl')
+
+# myModel = INSTRUCTOR('hkunlp/instructor-xl')
 
 
 def get_employee_embedding(employee):
@@ -22,17 +23,17 @@ def get_project_embedding(project):
   if project:
     proj_embedding = np.fromstring(project.embedding[1:-1], sep=' ')  # Convert the string back to a NumPy array
     return proj_embedding
-  return None   
-      
+  return None
+
+
+
 def get_embedding_from_resume(resume_text):
-  # resume_instruction = [["Represent the employee resume document for retrieving suitable projects: ",resume_text]]
-  resume_instruction = [["This is an employee's resume. I want to use this to assign them to suitable projects that they specialize in or have experience in. Here is the resume: ",resume_text]]
+  resume_instruction = [["Represent the employee resume document for retrieving suitable projects: ",resume_text]]
   embedding_resume = myModel.encode(resume_instruction)
   return embedding_resume[0]
 
 def get_embedding_from_project(project_text):
-  # project_instruction = [["Represent the project description for retrieval: ", project_text]]
-  project_instruction = [["This is a project my company is planning to work on. Represent this project description in a way which categorizes the skills required for it: ", project_text]]
+  project_instruction = [["Represent the project description for retrieval: ", project_text]]
   embedding_project = myModel.encode(project_instruction)
   return embedding_project[0]
 
@@ -74,12 +75,16 @@ def summarize_resume(resume_text):
     jobs = str(response['jobs'])
     return name, summary, skills, hobbies, jobs
 
+
+
+
+
 def main():
-    # prompt = "Hello World"
-    # model = GPT4QAModel()
-    # response = model.answer_question(prompt)
-    # print(f"response: {response}")
-   pass
+    prompt = "Hello, world!"
+    model = GPT4QAModel()
+    response = model.answer_question(prompt)
+    print(f"response: {response}")
+    # pass
 
 if __name__ == "__main__":
     main()
