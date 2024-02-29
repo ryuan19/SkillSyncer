86b3a7b9d37ad91c2b602eb0dd14081d17337c5c
krrishchawla <krrishchawla1@gmail.com>
2024-02-28 22:22:36 -0800

    Code clean, fix util functions for database management

--- util.py
+++ util.py
@@ -8,6 +8,8 @@ from json.decoder import JSONDecodeError
 from tqdm import tqdm
 from PyPDF2.errors import PdfReadError
 from InstructorEmbedding import INSTRUCTOR
+from models import User, Employee, Project
+
 
 
 myModel = INSTRUCTOR('hkunlp/instructor-xl')
@@ -75,7 +77,107 @@ def summarize_resume(resume_text):
     jobs = str(response['jobs'])
     return name, summary, skills, hobbies, jobs
 
+def update_best_employees(employee):
+    for project in Project.query.all():
+        project_embedding = np.fromstring(project.embedding[1:-1], sep=' ')
+        curr_best_employee = Employee.query.get(project.best_employee_id)
+        curr_best_employee_embedding = np.fromstring(curr_best_employee.embedding[1:-1], sep=' ')
+        employee_embedding = np.fromstring(employee.embedding[1:-1], sep=' ')
+        if cos_similarity(employee_embedding, project_embedding) > cos_similarity(curr_best_employee_embedding, project_embedding):
+            project.best_employee_id = employee.id
+            project.best_employee_name = employee.name
 
+def update_best_employees_llm_actuallyupdate(currguy, newguy, proj):
+    model = GPT4QAModel()
+    best_employee_prompt = "Here is some information about the best employee so far: \n" + makeEmployeePrompt(currguy) + "\n\n"
+    curr_employee_info = makeEmployeePrompt(newguy) #is actually the new guy
+    curr_employee_prompt = "Now, here is some information about the new employee we are evaluating: \n"+ curr_employee_info + "\n\n"
+    prompt = "Imagine you are a recruiter and you want to hire the best talent possible. You are looking at the best employee currently and a new employee applying for the postion. The new employee may or may not be better than the curent best employee.\n"
+    proj_prompt = "Here is some information about the project:\nProject Title: "+proj.title + "\nProject Description: "+proj.description + "\n\n"
+    end = "Looking at the current best employee and new employee applying, is the new employee better suited for this project compared to the current best employee? Output either \"Yes\" or \"No\". "
+
+    fullprompt = prompt + proj_prompt + best_employee_prompt + curr_employee_prompt + end
+    res = model.answer_question(fullprompt)
+    print("starthere2")
+    print(fullprompt)
+    printstr = "update_best_employees_llm_actuallyupdate : " + res
+    print(printstr)
+    if "yes" in res.lower(): #replace
+        print("update replace\n")
+        replace_prompt = best_employee_prompt + "Now, here is information about " + newguy.name + " who is a better candidate: \n" + curr_employee_info
+        replace_prompt += proj_prompt
+        replace_prompt += "\n Why is " + newguy.name + " the best suited candidate to this project? Respond in one to two sentences why " + newguy.name + "is the best candidate for this job."
+        reason = model.answer_question(replace_prompt)
+        proj.best_employee_id = newguy.id
+        proj.best_employee_name = newguy.name
+        proj.best_employee_reason = reason
+        db.session.commit() #update the reason
+
+
+def update_best_employees_llm(new_employee):
+    print("batman")
+    for project in Project.query.all():
+        curr_best_employee = Employee.query.get(project.best_employee_id) #guy must be in database
+        update_best_employees_llm_actuallyupdate(curr_best_employee,new_employee, project)
+
+
+def get_best_employee_id_name_for_project(embedding):
+  best_similarity = -1
+  best_employee= None
+
+  #project = Project.query.get(project_id)
+  for employee in Employee.query.all():
+    employee_embedding = get_employee_embedding(employee)
+    if employee_embedding is not None:
+        similarity = cos_similarity(embedding, employee_embedding)
+        if similarity > best_similarity:
+            best_similarity = similarity
+            best_employee= employee
+  return best_employee.id, best_employee.name
+
+def makeEmployeePrompt(employee):
+    temp1 = "Name: " + employee.name
+    temp2 = "\nID: " + str(employee.id)
+    temp3 = "\nUser ID: " + str(employee.user_id)
+    temp4 = "\nSummary: " + employee.summary
+    temp5= "\nHobbies: " + employee.hobbies
+    temp6 = "\nJobs: " +  employee.jobs
+    temp7 = "\nSkills: " + employee.skills
+    return temp1+temp2+temp3+temp4+temp5+temp6 +temp7
+
+def llm_get_best_employee_id_name_for_project(new_project):
+    model = GPT4QAModel()
+    best_employee = None
+    first = True
+    reason = ""
+    for employee in Employee.query.all():
+        if first or best_employee is None: #set best employee to first one
+            best_employee = employee
+            first = False
+            best_employee_prompt = "Here is some information about the employee in this role: \n" + makeEmployeePrompt(best_employee) + "\n\n"
+            best_employee_prompt += "Why is this person suited for the role? Answer in one to two sentences."
+            reason = model.answer_question(best_employee_prompt)
+        else:
+            best_employee_prompt = "Here is some information about the best employee so far: \n" + makeEmployeePrompt(best_employee) + "\n\n"
+            curr_employee_info = makeEmployeePrompt(employee)
+            curr_employee_prompt = "Now, here is some information about the new employee we are evaluating: \n"+ curr_employee_info + "\n\n"
+            prompt = "Imagine you are a recruiter and you want to hire the best talent possible. You are looking at the best employee currently and a new employee applying for the postion. The new employee may or may not be better than the curent best employee.\n"
+            proj_prompt = "Here is some information about the project:\nProject Title: "+new_project.title + "\nProject Description: "+new_project.description + "\n\n"
+            end = "Looking at the current best employee and new employee applying, is the new employee better suited for this project compared to the current best employee? Output either \"Yes\" or \"No\". "
+
+            fullprompt = prompt + proj_prompt + best_employee_prompt + curr_employee_prompt + end
+
+            res = model.answer_question(fullprompt)
+            print(employee.name, "Is this person better?:", res)
+            if "yes" in res.lower(): #replace
+                print("inhere\n")
+                best_employee = employee
+                replace_prompt = "Now, here is information about " + employee.name + " who is a better candidate: \n" + curr_employee_info
+                replace_prompt += proj_prompt
+                replace_prompt += "\n Why is " + employee.name + " the best suited candidate to this project? Respond in one to two sentences why " + employee.name + "is the best candidate for this job."
+                reason = model.answer_question(replace_prompt)
+
+    return best_employee, reason
 
 def main():
 
